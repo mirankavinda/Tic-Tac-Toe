090b9d12bab0fe96c35409225f4872936eb5d12e
Miran <mirankavinda12@gmail.com>
2023-12-23 14:05:58 +0530

    useRef hook and display winner in title

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react';
 import './TicTacToe.css'
 import circle_icon from '../assets/circle.png'
@@ -11,6 +11,7 @@ const TicTacToe = () => {
 
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
 
   const toggle = (e, num) => { 
     if (lock) {
@@ -26,53 +27,60 @@ const TicTacToe = () => {
       data[num] = "o";
       setCount(++count);
     }
+    checkWin();
   }
 
   const checkWin = () => { 
     if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data);
+      won(data[2]);
     } 
 
     else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
-      won(data);
+      won(data[5]);
     }
       
     else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
       
     else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     } 
 
     else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
-      won(data);
+      won(data[7]);
     } 
 
     else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
 
     else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
     
     else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data);
+      won(data[2]);
     }
 
     else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     } 
   }
 
   const won = (winner) => {
     setLock(true);
+    if (winner === "x") {
+      titleRef.current.innerHTML = `Congrats: <img src=${cross_icon} alt="cross" /> winner is cross`;
+    }
+    else {
+      titleRef.current.innerHTML = `Congrats: <img src=${circle_icon} alt="circle" /> winner is circle`;
+    }
   }
 
   return (
     <div className='container'>
-      <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
+      <h1 className="title" ref={titleRef}>Tic Tac Toe Game In <span>React</span></h1>
       <div className='board'>
         <div className="row1">
           <div className="boxes" onClick={(e)=>{toggle(e,0)}}></div>
