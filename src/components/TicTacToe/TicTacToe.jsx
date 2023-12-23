93f1ed3afd2c1f6d746be1a5c5d113faa667d0d6
Miran <mirankavinda12@gmail.com>
2023-12-23 12:53:59 +0530

    Add game title, board, and reset button to TicTacToe component

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './TicTacToe.css'
+import circle_icon from '../assets/circle.png'
+import cross_icon from '../assets/cross.png'
 
 const TicTacToe = () => {
   return (
-    <div>TicTacToe</div>
+    <div className='container'>
+      <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
+      <div className='board'>
+
+      </div>
+      <button className="reset">Reset</button>
+    </div>
   )
 }
 
