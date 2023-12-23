3b4ec34bb8e1bff0aa0738f901caed8e1df0f066
Miran <mirankavinda12@gmail.com>
2023-12-23 13:30:29 +0530

    Add Tic Tac Toe game layout and styling

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -3,12 +3,33 @@ import './TicTacToe.css'
 import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png'
 
+let data = ["", "", "", "", "", "", "", "", ""]
+
 const TicTacToe = () => {
+
+  
+
   return (
     <div className='container'>
       <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
       <div className='board'>
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
 
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
       </div>
       <button className="reset">Reset</button>
     </div>
